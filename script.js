ef0ee80c2bc2dbbbf7a1c2fa28d4be51073b746b
prtvi <prithvippv25@gmail.com>
2022-09-20 22:36:59 +0530

    add feature -portfolio -change tabs & show clicked img on design tab

--- script.js
+++ script.js
@@ -5,3 +5,51 @@ const toggleNavbar = function () {
 
 const navbar = document.querySelector('.navbar');
 navbar.addEventListener('click', toggleNavbar);
+
+// portfolio change tabs
+
+const btnTabs = document.querySelectorAll('.btn-tab');
+const tabs = document.querySelectorAll('.tab-content');
+
+btnTabs.forEach(btn => {
+	btn.addEventListener('click', function () {
+		// remove active class from all tab btns
+		btnTabs.forEach(bt => bt.classList.remove('active'));
+
+		tabs.forEach(tab => {
+			if (btn.dataset.btnTab === tab.dataset.tab) {
+				// if matched then add active class to tab btn
+				btn.classList.add('active');
+
+				//  display tab
+				tab.classList.add('active');
+
+				window.scrollTo(0, 0);
+			} else tab.classList.remove('active');
+		});
+	});
+});
+
+// adding functionality to design tab
+
+const designPreviewImg = document.querySelector('.design-preview-img');
+const hItemList = document.querySelector('.h-item-list');
+const hItems = document.querySelectorAll('.h-item');
+
+hItemList.addEventListener('click', function (e) {
+	const clickedItem = e.target.closest('.h-item');
+	if (!clickedItem) return;
+
+	const itemId = clickedItem.getAttribute('id');
+	const itemAlt = clickedItem.getAttribute('alt');
+
+	// remove active class from all and add to clicked item
+	hItems.forEach(i => i.classList.remove('active'));
+	clickedItem.classList.add('active');
+
+	// change src of preview image
+	designPreviewImg.src = `./assets/designs/${itemId}.png`;
+
+	// TODO
+	designPreviewImg.alt = `./assets/designs/${itemId}.png`;
+});
